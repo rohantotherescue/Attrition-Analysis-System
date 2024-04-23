e0c67966e71a3cfd36a78074ee616d475e1c7747
rohantotherescue <rohantotherescue@gmail.com>
2024-04-23 13:49:50 +0530

    Initial commit

---
+++ src/main/java/com/rohan/AttritionAnalysisSystem/AttritionAnalysisSystemApplication.java
@@ -0,0 +1,13 @@
+package com.rohan.AttritionAnalysisSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttritionAnalysisSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttritionAnalysisSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rohan/AttritionAnalysisSystem/AttritionAnalysisSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohan.AttritionAnalysisSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttritionAnalysisSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
